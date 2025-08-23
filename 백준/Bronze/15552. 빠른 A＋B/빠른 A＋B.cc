51b2ce2601cf17190089ebb7198b38bec8676e89
HyeongSeok Kim <gw904202@gmail.com>
2025-08-23 23:34:04 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 192 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a,b,c;
+    cin >> c;
+    for (int i = 0; i < c; i++)
+    {
+        cin >> a >> b;
+        cout << a+b << '\n';
+    }
+    
+}

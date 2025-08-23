649a363283ef7c938f3f8ce3a9a0a254b2d940d3
HyeongSeok Kim <gw904202@gmail.com>
2025-08-23 23:20:35 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    if(a==b && b==c){
+        cout << 10000+a*1000;
+    }
+    else if(a==b || b==c || a==c){
+        if(a==b){
+            cout << 1000+a*100;
+        }
+        else if(b==c){
+            cout << 1000+b*100;
+        }
+        else{
+            cout << 1000+a*100;
+        }
+    }
+    else{
+        cout << max(max(a,b),c)*100;
+    }
+    return 0;
+}

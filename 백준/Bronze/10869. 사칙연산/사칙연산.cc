a6ada9d3156a54c4f387fbbd220b751d93a3c567
HyeongSeok Kim <gw904202@gmail.com>
2025-08-23 23:31:06 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+main()
+{   
+    int a,b;
+
+    cin >> a;
+    cin >> b;
+
+    cout << a+b << '\n';
+    cout << a-b << '\n';
+    cout << a*b << '\n';
+    cout << a/b << '\n';
+    cout << a%b;
+
+}

dcd03c4e0cae29671fae2417c72998010461587c
HyeongSeok Kim <gw904202@gmail.com>
2025-12-10 14:17:15 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+    for(int i=1; i<=n; i++)
+        answer += (i%2==0 ? i : 0);
+    return answer;
+}

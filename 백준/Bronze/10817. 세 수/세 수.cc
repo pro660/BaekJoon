897bc8c68963c85b53cf8db5a4b729a3209c464b
HyeongSeok Kim <gw904202@gmail.com>
2026-04-13 16:25:21 +0900

    [Bronze IV] Title: 세 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    vector<int> v = {a,b,c};
+    sort(v.begin(), v.end());
+    printf("%d\n", v[1]);
+    
+    return 0;
+}
